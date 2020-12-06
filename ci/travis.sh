38cfae1e88c5458c953357c62122bdf03bfa8af9
TpaeFawzen <30316430+GH-TpaeFawzen@users.noreply.github.com>
2020-12-06 21:04:49 +0900

    ci/travis.sh: Not necessarily need to be bash
    case'd; tr'd.
    
    Also I thought this code lacks some readability because of not streamlily readable because of too many functions so I thought of changing the code completely but did not.

--- ci/travis.sh
+++ ci/travis.sh
@@ -1,36 +1,36 @@
-#!/bin/bash -e
+#!/bin/sh -eu
 # The main script for building/testing while under travis ci.
 # https://travis-ci.org/libgd/libgd
 
 travis_fold() {
-	if [[ -n ${TRAVIS_OS_NAME} ]] ; then
-		printf 'travis_fold:%s:%s\r\n' "$@" | sed 's: :_:g'
-	fi
+	case "${TRAVIS_OS_NAME}" in ?*)
+		printf 'travis_fold:%s:%s\r\n' "$@" | tr ' ' '_'
+	esac
 }
 
-if [[ -n ${TRAVIS_OS_NAME} ]] ; then
+case "${TRAVIS_OS_NAME}" in ?*)
 	whitebg=$(tput setab 7)
 	blackfg=$(tput setaf 0)
 	normal=$(tput sgr0)
-else
+;;(*)
 	whitebg=
 	blackbg=
 	normal=
-fi
+esac
 v() {
 	local fold=""
 	case $1 in
 	--fold=*) fold=${1:7}; shift;;
 	esac
-	if [[ -n ${fold} ]] ; then
+	case "${fold}" in ?*)
 		travis_fold start "${fold}"
 		echo "\$ $*"
 		"$@"
 		travis_fold end "${fold}"
-	else
+	;; *)
 		echo "${whitebg}${blackfg}\$ $*${normal}"
 		"$@"
-	fi
+	esac
 }
 
 ncpus=$(getconf _NPROCESSORS_ONLN)
